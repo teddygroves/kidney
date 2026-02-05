f9e93522b10c0b7ff1e8376f4b0cbc454d71c82c
Teddy Groves <groves.teddy@gmail.com>
2026-02-05 16:45:19 +0100

    Add baseline analyses

--- scripts/run_mcmc.py
+++ scripts/run_mcmc.py
@@ -2,32 +2,30 @@ from pathlib import Path
 import bambi as bmb
 import polars as pl
 
-from kidney.model_configuration import ANALYSIS_TO_OUTCOME, ANALYSIS_TO_FORMULA
-from kidney.paths import PREPARED_DIR, RESULTS_DIR
+from kidney.model_configuration import MODEL_CONFIGS
+from kidney.paths import RESULTS_DIR
 
 
 ROOT = Path(__file__).parent.parent
 
 
 def main():
-    for analysis, outcome in ANALYSIS_TO_OUTCOME.items():
-        formula = ANALYSIS_TO_FORMULA[analysis]
-        print(f"Analysing {analysis} data (outcome column: {outcome})")
-        msts = pl.read_csv(PREPARED_DIR / f"{analysis}.csv")
-        for lin_or_log, ycol in (("lin", outcome), ("log", "log_" + outcome)):
-            bmb_formula = bmb.Formula(formula.format(y=ycol))
-            model = bmb.Model(formula=bmb_formula, data=msts.to_pandas())
-            idata = model.fit(
-                target_accept_prob=0.95,
-                idata_kwargs={"log_likelihood": True},
-            )
-            model.predict(
-                idata,
-                data=msts.to_pandas(),
-                kind="response",
-                inplace=True,
-            )
-            idata.to_netcdf(RESULTS_DIR / f"idata_{analysis}_{lin_or_log}.nc")
+    for mc in MODEL_CONFIGS:
+        print(f"Analysing {mc.name} data (outcome column: {mc.outcome})")
+        msts = pl.read_csv(mc.csv)
+        bmb_formula = bmb.Formula(mc.formula.format(y=mc.outcome))
+        model = bmb.Model(formula=bmb_formula, data=msts.to_pandas())
+        idata = model.fit(
+            target_accept_prob=0.95,
+            idata_kwargs={"log_likelihood": True},
+        )
+        model.predict(
+            idata,
+            data=msts.to_pandas(),
+            kind="response",
+            inplace=True,
+        )
+        idata.to_netcdf(RESULTS_DIR / f"idata_{mc.name}.nc")
 
 
 if __name__ == "__main__":
--- src/kidney/model_configuration.py
+++ src/kidney/model_configuration.py
@@ -1,18 +1,42 @@
-DEFAULT_FORMULA = "{y} ~ (1|rat) + gtyp + age + sex + gtyp:age + gtyp:sex"
+from dataclasses import dataclass
+from pathlib import Path
+
+from kidney.paths import PREPARED_FILES as PF
+
+
+@dataclass
+class ModelConfig:
+    name: str
+    formula: str
+    csv: Path
+    outcome: str
+
+
+FORMULA = "{y} ~ (1|rat) + gtyp + age + sex + gtyp:age + gtyp:sex"
+FORMULA_GAS = (
+    "{y} ~ (1|rat) + gtyp + age + sex + gtyp:age + gtyp:sex + gtyp:age:sex"
+)
 FORMULA_BP = "{y} ~ gtyp + age + sex + gtyp:age + gtyp:sex"
-ANALYSIS_TO_FORMULA = {
-    "bfi_vehicle": DEFAULT_FORMULA,
-    "bfi_empa": DEFAULT_FORMULA,
-    "bp_vehicle": FORMULA_BP,
-    "bp_empa": FORMULA_BP,
-    "frequency": DEFAULT_FORMULA,
-    "power": DEFAULT_FORMULA,
-}
-ANALYSIS_TO_OUTCOME = {
-    "bfi_vehicle": "bfi_change",
-    "bfi_empa": "bfi_change",
-    "bp_vehicle": "bp_change",
-    "bp_empa": "bp_change",
-    "frequency": "frequency_change",
-    "power": "power_change",
-}
+MODEL_CONFIGS = (
+    ModelConfig("bfi_vehicle", FORMULA, PF["bfi_vehicle"], "log_bfi_change"),
+    ModelConfig("bfi_empa", FORMULA, PF["bfi_empa"], "log_bfi_change"),
+    ModelConfig("bfi_baseline", FORMULA, PF["bfi_vehicle"], "log_bfi_baseline"),
+    ModelConfig(
+        "bfi_baseline_extra_interaction",
+        FORMULA_GAS,
+        PF["bfi_vehicle"],
+        "log_bfi_baseline",
+    ),
+    ModelConfig("bp_vehicle", FORMULA_BP, PF["bp_vehicle"], "log_bp_change"),
+    ModelConfig("bp_empa", FORMULA_BP, PF["bp_empa"], "log_bp_change"),
+    ModelConfig("bp_baseline", FORMULA_BP, PF["bp_vehicle"], "log_bp_baseline"),
+    ModelConfig("frequency", FORMULA, PF["frequency"], "frequency_change"),
+    ModelConfig(
+        "frequency_baseline",
+        FORMULA,
+        PF["frequency"],
+        "frequency_vehicle",
+    ),
+    ModelConfig("power", FORMULA, PF["power"], "log_power_change"),
+    ModelConfig("power_baseline", FORMULA, PF["power"], "log_power_vehicle"),
+)
