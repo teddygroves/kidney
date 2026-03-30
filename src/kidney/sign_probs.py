1031897a099c8c6995cc8fbcbccff6788dd7e3c8
Oleg Zhukov <lampochka@Olegs-MacBook-Pro.local>
2026-03-30 09:55:19 +0200

    Removed unnecessary arg from get_signif func

--- src/kidney/sign_probs.py
+++ src/kidney/sign_probs.py
@@ -1,7 +1,11 @@
 def get_sign_probs(ts):
     return {k: (v>0).mean().item() for k, v in ts.items()}
 
-def get_signif(sp, thresh_prob=0.025):
+def get_signif(sp):
+    "Return '*' if sp<0.025 or sp>0.975"
+    
+    thresh_prob = 0.025
+    
     if sp == 0.5:
         return ''
     if sp > 0.5:
